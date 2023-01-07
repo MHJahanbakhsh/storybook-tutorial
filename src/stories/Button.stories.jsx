7d362700dde03c5a932abd1e92a19c7f66462a74
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2023-01-07 13:57:18 +0330

    before adding stack

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,8 +1,43 @@
 import Button from '../components/Button'
 
 export default {
-    title: "Button",
-    component: Button
+    title: "Components/Button",
+    component: Button,
+    argTypes:{handleClick:{action:'handleClick'}}
 }
 
-export const Red = ()=><Button label='press me' backgroundColor="red"/>
+const Template = args=> <Button {...args}/>
+
+export const Red = Template.bind({})
+Red.args = {
+    backgroundColor:'red',
+    label:'press me',
+    size:'md'
+}
+export const Green = Template.bind({})
+Green.args = {
+    backgroundColor:'Green',
+    label:'press me',
+    size:'md'
+}
+
+export const Small = Template.bind({})
+Small.args = {
+    backgroundColor:'red',
+    label:'press me',
+    size:'sm'
+}
+
+export const Large = Template.bind({})
+Large.args = {
+    backgroundColor:'Green',
+    label:'press me',
+    size:'lg'
+}
+
+export const LargeLabel = Template.bind({})
+LargeLabel.args = {
+    backgroundColor:'Green',
+    label:'press mescjndfkhsdkfsdkfgbdfjksdfbj jkfsdfvjh',
+    size:'md'
+}
